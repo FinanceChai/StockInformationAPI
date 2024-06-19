a47bd4275bc77434a81e3b14cbe7c09dae8e2af5
FinanceChai <50843817+FinanceChai@users.noreply.github.com>
2024-06-19 11:18:56 +0100

    Update stockinformation.js

--- functions/stockinformation.js
+++ functions/stockinformation.js
@@ -1,8 +1,8 @@
-const express = require('express')
-const serverless = require('serverless-http')
-const axios = require('axios')
-const cheerio = require('cheerio')
-const app = express()
+const express = require('express');
+const serverless = require('serverless-http');
+const axios = require('axios');
+const cheerio = require('cheerio');
+const app = express();
 const router = express.Router();
 
 const { getLastFridayOrNonHolidayDate, dateToUnixTimestampPlusADay, dateToUnixTimestamp, formatDateToMatchApiArgument } = require('./helpers.js');
@@ -10,101 +10,47 @@ const { getLastFridayOrNonHolidayDate, dateToUnixTimestampPlusADay, dateToUnixTi
 const headers = {
     'User-Agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Mobile Safari/537.36',
 }
-  
+
 router.get('/', (req, res) => {
-    res.json('Welcome to the stock information API')
-})
+    res.json('Welcome to the cryptocurrency funding rates API');
+});
 
-router.get('/return/:symbol/:firstDate/:secondDate', async (req, res) => {
-    
-    try{
-        var startDateValue = 0;
-        var endDateValue = 0;
-        var result = 0.0;
+router.get('/fundingrate/:symbol', async (req, res) => {
+    try {
+        const { symbol } = req.params;
+        const url = `https://www.coinglass.com/FundingRate/${symbol}`;
 
-        const { symbol, firstDate, secondDate } = req.params;
-
-        var startDate = getLastFridayOrNonHolidayDate(firstDate);
-        var endDate = getLastFridayOrNonHolidayDate(secondDate);
-
-        const url = 'https://finance.yahoo.com/quote/' + symbol + '/history/?period1=' + dateToUnixTimestamp(startDate) +'&period2=' + dateToUnixTimestampPlusADay(endDate);
-
-        const response = await axios.get(url, {headers,});
+        const response = await axios.get(url, { headers });
         const html = response.data;
-    
         const $ = cheerio.load(html);
 
-        const processRows = () => {
-            $('tbody tr.svelte-ewueuo').each((index, element) => {
-                const row = $(element);
-                const dateCell = row.find('td:nth-child(1)').text();
+        const fundingRates = [];
 
-                if (formatDateToMatchApiArgument(dateCell) == startDate) {
-                    startDateValue = parseFloat(row.find('td:nth-child(2)').text());
-                }
+        $('.table-container table tbody tr').each((index, element) => {
+            const row = $(element);
+            const exchange = row.find('td:nth-child(1)').text().trim();
+            const fundingRate = row.find('td:nth-child(2)').text().trim();
+            const estimatedRate = row.find('td:nth-child(3)').text().trim();
+            const rateChange = row.find('td:nth-child(4)').text().trim();
 
-                if (formatDateToMatchApiArgument(dateCell) === endDate) {
-                    endDateValue = parseFloat(row.find('td:nth-child(2)').text());
-                }
+            fundingRates.push({
+                exchange,
+                fundingRate,
+                estimatedRate,
+                rateChange
             });
+        });
 
-            if (startDateValue !== 0 && endDateValue !== 0) {
-                result = ((endDateValue - startDateValue) / startDateValue) * 100;
-                res.json(result.toFixed(2));
-            } else {
-                console.log('Start date or end date not found.');
-                res.status(404).send('Start date or end date not found.');
-            }
-        };
-
-        processRows();
-
-    } catch(error) {
+        if (fundingRates.length > 0) {
+            res.json(fundingRates);
+        } else {
+            res.status(404).send('Funding rates not found.');
+        }
+    } catch (error) {
         console.error('Error:', error);
         res.status(500).send('Internal Server Error');
     }
-})
-
-
-router.get('/dividend/:symbol', (req, res) => {
-    
-    const symbol = req.params.symbol
-
-    const url = 'https://www.streetinsider.com/dividend_history.php?q='
-
-
-    axios.get(url + symbol)
-        .then(response => {
-            const html = response.data
-            const $ = cheerio.load(html)
-            const parsedData = []
-            const dividendData = []
-
-            $('td', html).each(function () {
-                const data = $(this).text()
-
-                dividendData.push({
-                    data
-                })
-            })
-
-            for (i = 0; i < dividendData.length; i=i+9) {
-
-                var dividend = {
-                    ExDivDate: JSON.stringify(dividendData[i]).split(':')[1].slice(1,-2),
-                    Amount: JSON.stringify(dividendData[i+1]).split(':')[1].slice(1,-2),
-                    DeclarationDate: JSON.stringify(dividendData[i+5]).split(':')[1].slice(1,-2),
-                    RecordDate: JSON.stringify(dividendData[i+6]).split(':')[1].slice(1,-2),
-                    PaymentDate: JSON.stringify(dividendData[i+7]).split(':')[1].slice(1,-2),
-                }
-
-                parsedData.push(dividend)
-              } 
-
-            res.json(parsedData)
-
-        }).catch(err => console.log(err))
-})
+});
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
@@ -112,19 +58,15 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Credentials', true);
     if (req.method === 'OPTIONS') {
-      res.sendStatus(200);
+        res.sendStatus(200);
     } else {
-      next();
+        next();
     }
-  });
-  
+});
 
-app.use('/.netlify/functions/stockinformation', router)
+app.use('/.netlify/functions/cryptoinfo', router);
 
+module.exports.handler = serverless(app);
 
-
-module.exports.handler=serverless(app)
-
-//remove commented code from below for local testing
-//module.exports = router;
-
+// Remove commented code from below for local testing
+// module.exports = router;
